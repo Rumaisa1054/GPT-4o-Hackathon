d483f3cdaabb30040d9ab0c9142a4a73c856ea0d
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2024-06-02 22:42:57 +0500

    Update hackathon_api.py

--- hackathon_api.py
+++ hackathon_api.py
@@ -2,7 +2,7 @@ import replicate
 import os
 
 
-os.environ['REPLICATE_API_TOKEN'] = os.getenv('MINE')
+os.environ['REPLICATE_API_TOKEN'] = os.getenv('MY_KEY')
 
 use_model = "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"
 
